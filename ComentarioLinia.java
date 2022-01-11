46cadd8d44d511a7df3fb9cf57387775758179cb
Eloy Altozano <yole.tres@gmail.com>
2022-01-11 11:26:58 +0100

    Ejercicio ComentarioLinia Eloy Altozano

---
+++ ComentarioLinia.java
@@ -0,0 +1,9 @@
+//Eloy Altozano Meijer
+public class ComentarioLinia{
+	
+	public static void main(String[] args){
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
